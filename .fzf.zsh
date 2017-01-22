135a7504a59c2e7b5fbf48ff18da71eb89381f7e
AJ <aj@laatu.uk>
2017-01-22 11:53:50 +0000

    change fzf default opt

--- .fzf.zsh
+++ .fzf.zsh
@@ -8,4 +8,4 @@ source "$HOME/.fzf/shell/key-bindings.zsh"
 
 # ------------
 alias fzf="fzf-tmux -d 7"
-export FZF_DEFAULT_OPTS="--reverse --inline-info"
+export FZF_DEFAULT_OPTS="--reverse --inline-info --preview 'file --mime-type {} | sift -q text/plain && cat {} || echo blob' --preview-window right:60%:hidden --bind \?:toggle-preview --bind pgup:preview-page-up --bind pgdn:preview-page-down"
