475a9dac50637993b7083090d147bfc0de449586
AJ <aj@48k.io>
2018-04-01 21:23:36 +0100

    add alias for fzf / git branch select

--- .fzf.zsh
+++ .fzf.zsh
@@ -9,3 +9,11 @@ source "$HOME/.fzf/shell/key-bindings.zsh"
 # ------------
 alias fzf="fzf-tmux -d 7"
 export FZF_DEFAULT_OPTS="--cycle --multi --reverse --inline-info --preview 'file --mime-type {} | sift -q text/plain && cat {} || echo blob' --preview-window right:60%:hidden --bind \?:toggle-preview --bind pgup:preview-page-up --bind pgdn:preview-page-down"
+
+bb() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
